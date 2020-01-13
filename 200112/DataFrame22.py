dea59e55dbfe22c49007340ab80a9c24dba5a0e4
AnKiWoong <ankiwoong@gmail.com>
2020-01-13 19:49:03 +0900

    master

--- 200112/DataFrame22.py
+++ 200112/DataFrame22.py
@@ -4,6 +4,6 @@ from Data import grade_dic
 # 데이터프레임 생성
 df = DataFrame(grade_dic, index=['노진구', '이슬이', '비실이', '퉁퉁이', '도라에몽'])
 
-# 전치(가로와 세로가 바뀐 형태) 구하기
+# 전치(열과 컬럼이 바뀐 형태) 구하기
 df_t = df.T
 print(df_t)
