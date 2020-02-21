d1520b4cd905ccbe7007095e94905420f9cfd281
AnKiWoong <ankiwoong@gmail.com>
2020-02-21 13:57:28 +0900

    master

---
+++ 200223/ch2/pivot_table5.py
@@ -0,0 +1,53 @@
+'''
+Pandas와 Pivot의 연계
+'''
+from print_df import print_df
+from pandas import DataFrame
+from matplotlib import pyplot as plt
+import numpy as np
+from sample_data import traffic
+
+# 데이터 수집
+df = DataFrame(traffic)
+# print_df(df)
+
+# 데이터 전처리
+# '년도'에 대한 컬럼만 리스트로 추출
+year = list(df['year'])
+# print_df(year)
+
+# 빈 딕셔너리 생성
+new_name = {}
+
+# '년도' 리스트에 대해 반복
+for i, v in enumerate(year):
+    new_name[i] = v
+# print(new_name)
+
+# 데이터 프레임의 인덱스 변경
+df.rename(index=new_name, inplace=True)
+
+# 기존의 '년도' 컬럼은 삭제
+df.drop('year', axis=1, inplace=True)
+
+# 컬럼명 변경
+df.rename(
+    columns={
+        'car_vs_people': '차 대 사람', 'car_vs_car': '차 대 차', 'car_only': '차량 단독',
+    }, inplace=True
+)
+
+print_df(df)
+
+# 전역 설정
+plt.rcParams["font.family"] = 'NanumGothic'         # 한글 폰트
+plt.rcParams["font.size"] = 12                      # 폰트 사이즈
+plt.rcParams["figure.figsize"] = (12, 8)           # 그래프 사이즈
+'''
+산점도 그래프
+'''
+df.plot.scatter(x='차 대 사람', y='차 대 차')
+plt.grid()
+plt.title('차 대 사람과 차 대 차의 교통사고량 관계')
+plt.show()
+plt.close()
