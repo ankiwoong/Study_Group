d4bb61a779a24a97718df659c0cc759c63911e0a
AnKiWoong <ankiwoong@gmail.com>
2020-03-30 20:32:45 +0900

    SQL Study

---
+++ SQL/DataInsert_1.sql
@@ -0,0 +1,7 @@
+INSERT INTO `topic` (
+title,
+description,
+author,
+created
+) VALUES
+('about javascript', 'javascript is ~', 'ankiwoong', '2015-4-10 12:20:5');
---
+++ SQL/DataInsert_2.sql
@@ -0,0 +1,7 @@
+INSERT INTO `topic` (
+title,
+description,
+author,
+created
+) VALUES
+('variable and constant', 'variable is ~', 'ankiwoong', '2015-4-15 12:20:5');
