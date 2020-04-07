bc0b4563307630b179a2ee41eaebbaff6dd82a77
ankiwoong <ankiwoong@gmail.com>
2020-04-08 08:41:00 +0900

    Python Basic Code

---
+++ 200408/tuple.py
@@ -0,0 +1,20 @@
+# Sequence Structure Container(시퀀스 구조 컨테이너)
+# empty tuple
+a = ()
+print(a)
+print('-' * 30)
+
+# tuple packing
+alphabet = 'b', 'c', 'd', 'e'
+print(alphabet)
+print('-' * 30)
+
+# tuple unpacking
+f, g, h, i = 10, 20, 30, 40
+print(f, g, h, i)
+print('-' * 30)
+
+# list -> tuple
+j = ['indigo', 'Central']
+print(tuple(j))
+print('-' * 30)
