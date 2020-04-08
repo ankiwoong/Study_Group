cbe0cff468d6ad36c3681a72531f945e103e6d04
ankiwoong <ankiwoong@gmail.com>
2020-04-08 11:19:33 +0900

    Python Basic Code

---
+++ 200408/comparison_operators.py
@@ -0,0 +1,26 @@
+a = 5
+b = 10
+
+# less than
+print(a < b)
+print('-' * 30)
+
+# less than equal to
+print(a <= b)
+print('-' * 30)
+
+# equal to
+print(a == b)
+print('-' * 30)
+
+# greater than equal to
+print(a >= b)
+print('-' * 30)
+
+# greater than
+print(a > b)
+print('-' * 30)
+
+# not equal to
+print(a != b)
+print('-' * 30)
---
+++ 200408/for_list.py
@@ -0,0 +1,15 @@
+# for
+for i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    print(i)
+print('-' * 30)
+
+# range for
+for i in range(0, 10):
+    print(i)
+print('-' * 30)
+
+# len for
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i in range(0, len(a)):
+    print(i)
+print('-' * 30)
