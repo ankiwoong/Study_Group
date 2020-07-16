216506ba50fe1a9e436e4feba98cb7c1fb1e1595
Ankiwoong <ankiwoong@gmail.com>
2020-07-16 14:09:31 +0900

    오른쪽 알파벳 삼각형

---
+++ 200716/characters_pattern1.py
@@ -0,0 +1,11 @@
+def pattern(n):
+    x = 65
+    for i in range(0, n):
+        ch = chr(x)
+        x += 1
+        for j in range(0, i + 1):
+            print(ch, end=" ")
+        print("\r")
+
+
+pattern(5)
