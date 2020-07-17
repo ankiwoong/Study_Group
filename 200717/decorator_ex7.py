8d4d3cac2c44830e0ea47b4ac61bc5d2f6fd1a35
Ankiwoong <ankiwoong@gmail.com>
2020-07-17 10:31:27 +0900

    디코딩 된 기능에서 값 반환

---
+++ 200717/decorator_ex7.py
@@ -0,0 +1,14 @@
+def function1(function):
+    def wrapper(*args, **kwargs):
+        function(*args, **kwargs)
+        print("it worked")
+
+    return wrapper
+
+
+@function1
+def function2(name):
+    print(f"{name}")
+
+
+function2("python")
