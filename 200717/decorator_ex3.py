37aab8cd97dd8f25b3c06c41c79addcd09e1f90a
Ankiwoong <ankiwoong@gmail.com>
2020-07-17 09:23:51 +0900

    함수에서 함수로 반환

---
+++ 200717/decorator_ex3.py
@@ -0,0 +1,17 @@
+def func(n):
+    def func1():
+        return "edureka"
+
+    def func2():
+        return "python"
+
+    if n == 1:
+        return func1
+    else:
+        return func2
+
+
+a = func(1)
+b = func(2)
+print(a())
+print(b())
