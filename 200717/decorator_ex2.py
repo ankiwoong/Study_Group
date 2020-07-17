075166709c65261bc9487590046e3ef32a829b95
Ankiwoong <ankiwoong@gmail.com>
2020-07-17 09:21:00 +0900

    내부 함수

---
+++ 200717/decorator_ex2.py
@@ -0,0 +1,14 @@
+def func():
+    print("first function")
+
+    def func1():
+        print("first child function")
+
+    def func2():
+        print(" second child function")
+
+    func1()
+    func2()
+
+
+func()
