2b345d8572f005996ad74c42bcb7bf61ac5a6643
Ankiwoong <ankiwoong@gmail.com>
2020-07-24 22:07:41 +0900

    study quiz

---
+++ 200724/quiz 1.py
@@ -0,0 +1,7 @@
+# problem_list = ["인성", "코딩", "파이썬", "계산", "결과"] 리스트를 만들고 for 로 반복하여 problem_list 각 요소에 "문제 있어?"를 더하여 출력해보자
+
+problem_list = ["인성", "코딩", "파이썬", "계산", "결과"]
+
+for i, v in enumerate(problem_list):
+    print(i, v, "문제 있어?")
+
