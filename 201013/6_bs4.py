f478ff8cb54148aa4fe21500b688d1ab9079e836
AnKiWoong <ankiwoong@gmail.com>
2020-10-14 07:14:49 +0900

    BeautifulSoup4 기본 2

--- 201013/6_bs4.py
+++ 201013/6_bs4.py
@@ -24,4 +24,29 @@ soup = BeautifulSoup(res.text, "lxml")
 
 # print(soup.find("li", attrs={"class": "rank01"}))
 rank1 = soup.find("li", attrs={"class": "rank01"})
-print(rank1.a)
+# print(rank1.a)
+# print(rank1.a.get_text())
+# print(rank1.next_sibling)
+
+rank2 = rank1.next_sibling.next_sibling
+rank3 = rank2.next_sibling.next_sibling
+# print(rank3.a.get_text())
+
+rank2 = rank3.previous_sibling.previous_sibling
+# print(rank2.a.get_text())
+
+# print(rank1.parent)
+
+rank2 = rank1.find_next_sibling("li")
+# print(rank2.a.get_text())
+
+rank3 = rank2.find_next_sibling("li")
+# print(rank3.a.get_text())
+
+rank2 = rank3.find_previous_sibling("li")
+# print(rank2.a.get_text())
+
+# print(rank1.find_next_siblings("li"))
+
+webtoon = soup.find("a", text="고수-2부 118화")
+print(webtoon)
