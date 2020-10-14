4c3c2bb8eda7558e63f94a88187124de9b3f8f06
AnKiWoong <ankiwoong@gmail.com>
2020-10-15 07:05:14 +0900

    BeautifulSoup4 활용 1-2

--- 201014/8_bs4_gauss.py
+++ 201014/8_bs4_gauss.py
@@ -13,8 +13,21 @@ cartoons = soup.find_all("td", attrs={"class": "title"})
 # print(title)
 # print("https://comic.naver.com" + link)
 
-for cartoon in cartoons:
-    title = cartoon.a.get_text()
-    link = "https://comic.naver.com" + cartoon.a["href"]
+# 만화 제목 + 링크 가져오기
+# for cartoon in cartoons:
+# title = cartoon.a.get_text()
+# link = "https://comic.naver.com" + cartoon.a["href"]
 
-    print(title, link)
+# print(title, link)
+
+# 평점 구하기
+total_rates = 0
+cartoons = soup.find_all("div", attrs={"class": "rating_type"})
+
+for cartoon in cartoons:
+    rate = cartoon.find("strong").get_text()
+    print(rate)
+    total_rates += float(rate)
+
+print("전체 점수 : ", total_rates)
+print("평균 점수 : ", total_rates / len(cartoons))
