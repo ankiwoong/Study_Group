7fa351a37d974a11970d13d3128ed3004ae13d1a
AnKiWoong <ankiwoong@gmail.com>
2020-10-17 07:52:01 +0900

    BeautifulSoup4 활용 3-2

--- 201015/11_daum_movies.py
+++ 201015/11_daum_movies.py
@@ -1,19 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get(
-    "https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q=2019%EB%85%84+%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84"
-)
-res.raise_for_status()
 
-soup = BeautifulSoup(res.text, "lxml")
-# print(soup)
+for year in range(2015, 2020):
+    url = "https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q={}%EB%85%84+%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84".format(
+        year
+    )
 
-images = soup.find_all("img", attrs={"class": "thumb_img"})
+    res = requests.get(url)
+    res.raise_for_status()
 
-for image in images:
-    # print(image["src"])
-    image_url = image["src"]
-    if image_url.startswith("//"):
-        image_url = "https:" + image_url
-    print(image_url)
+    soup = BeautifulSoup(res.text, "lxml")
+    # print(soup)
+
+    images = soup.find_all("img", attrs={"class": "thumb_img"})
+
+    for idx, image in enumerate(images):
+        # print(image["src"])
+        image_url = image["src"]
+        if image_url.startswith("//"):
+            image_url = "https:" + image_url
+        # print(image_url)
+
+        image_res = requests.get(image_url)
+        image_res.raise_for_status()
+
+        with open(
+            "D:\\Code\\Study\\Study_Group\\result\\movie_{}_{}.jpg".format(
+                year, idx + 1
+            ),
+            "wb",
+        ) as f:
+            f.write(image_res.content)
+
+        if idx >= 4:  # 상위 5개 이미지까지만 다운로드
+            break
