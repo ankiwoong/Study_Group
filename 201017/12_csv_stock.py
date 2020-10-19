1a1687bf3de1a734ddedc82f9d0536fd0ba19f8f
AnKiWoong <ankiwoong@gmail.com>
2020-10-20 08:20:08 +0900

    CSV 기본 2

--- 201017/12_csv_stock.py
+++ 201017/12_csv_stock.py
@@ -2,9 +2,19 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok="
+url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=&page="
 
-for page in range(1, 2):
+filename = "시가총액1-200.csv"
+f = open(filename, "w", encoding="utf-8-sig", newline="")
+writer = csv.writer(f)
+
+title = "N	종목명	현재가	전일비	등락률	액면가	시가총액	상장주식수	외국인비율	거래량	PER	ROE".split(
+    "\t"
+)  # ['N', '종목명', '현재가', '전일비', '등락률', '액면가', '시가총액', '상장주식수', '외국인비율', '거래량', 'PER', 'ROE']
+# print(type(title))  # <class 'list'>
+writer.writerow(title)
+
+for page in range(1, 5):
     res = requests.get(url + str(page))
     res.raise_for_status()
 
@@ -22,4 +32,6 @@ for page in range(1, 2):
             continue
 
         data = [column.get_text().strip() for column in columns]
-        print(data)
+        # print(data)
+
+        writer.writerow(data)
