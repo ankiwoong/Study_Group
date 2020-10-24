0e91aacd0173150fd596f7815b850f797ee3845c
AnKiWoong <ankiwoong@gmail.com>
2020-10-24 09:37:41 +0900

    Selenium 활용 1-2

--- 201023/15_selenium_Flight.py
+++ 201023/15_selenium_Flight.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 browser = webdriver.Chrome("./chromedriver.exe")
 browser.maximize_window()  # 창 최대화
@@ -27,6 +30,16 @@ browser.find_element_by_xpath('//*[@id="recommendationList"]/ul/li[1]').click()
 # 항공권 검색
 browser.find_element_by_link_text("항공권 검색").click()
 
+try:
+    elem = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="content"]/div[2]/div/div[4]/ul/li[1]')
+        )
+    )  # 성공했을 때 동작 수행
+    print(elem.text)  # 첫 번째 결과 출력
+finally:
+    browser.quit()
+
 # 첫번째 결과 출력
-elem = browser.find_element_by_xpath('//*[@id="content"]/div[2]/div/div[4]/ul/li[1]')
-print(elem.text)
+# elem = browser.find_element_by_xpath('//*[@id="content"]/div[2]/div/div[4]/ul/li[1]')
+# print(elem.text)
