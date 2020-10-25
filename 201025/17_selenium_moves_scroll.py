19b7804ec16e6ab8ed1eb7a85cac21f145063196
AnKiWoong <ankiwoong@gmail.com>
2020-10-25 10:47:07 +0900

    Selenium 활용 2-3

--- 201025/17_selenium_moves_scroll.py
+++ 201025/17_selenium_moves_scroll.py
@@ -1,27 +1,5 @@
-# import requests
-# from bs4 import BeautifulSoup
-
-# url = "https://play.google.com/store/movies/top"
-# headers = {
-#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Whale/2.8.107.16 Safari/537.36",
-#     "Accept-Language": "ko-KR, ko",
-# }
-
-# res = requests.get(url, headers=headers)
-# res.raise_for_status()
-# soup = BeautifulSoup(res.text, "lxml")
-
-# movies = soup.find_all("div", attrs={"class": "ImZGtf mpg5gc"})
-# # print(len(movies))
-
-# # with open("movie.html", "w", encoding="utf8") as f:
-# # f.write(res.text)
-# # f.write(soup.prettify())  # html 문서 이쁘게 출력
-
-# for movie in movies:
-#     title = movie.find("div", attrs={"class": "WsMG1c nnK0zc"}).get_text()
-# print(title)
-
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
@@ -61,3 +39,28 @@ while True:
     prev_height = curr_height
 
 print("스크롤 완료")
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+# movies = soup.find_all("div", attrs={"class": ["ImZGtf mpg5gc", "Vpfmgd"]})
+movies = soup.find_all("div", attrs={"class": "Vpfmgd"})
+print(len(movies))
+
+for movie in movies:
+    title = movie.find("div", attrs={"class": "WsMG1c nnK0zc"}).get_text()
+    print(title)
+
+    # 할인 전 가격
+    original_price = movie.find("span", attrs={"class": "SUZt4c djCuy"})
+    if original_price:
+        original_price = original_price.get_text()
+    else:
+        print(title, " <할인되지 않은 영화 제외")
+        continue
+
+    # 할인 된 가격
+    price = movie.find("span", attrs={"class": "VfPpfd ZdBevf i5DZme"})
+
+    # 링크
+    link = movie.find("a", attrs={"class": "JC71ub"})["href"]
+    # 올바른 링크 : https://play.google.com + link
