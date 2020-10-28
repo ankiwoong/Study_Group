1fe2e2318733d21c2e6833110c8c672493a31352
AnKiWoong <ankiwoong@gmail.com>
2020-10-29 08:58:40 +0900

    네이버 웹툰 제목 가져오기 - 요일별 영역 가져오기

--- 201029/code.py
+++ 201029/code.py
@@ -7,21 +7,6 @@ html = requests.get("http://comic.naver.com/webtoon/weekday.nhn")
 soup = BeautifulSoup(html.text, "html.parser")
 html.close()
 
-# 월요웹툰영역 추출하기
-data1 = soup.find("div", {"class": "col_inner"})
-# pprint(data1)
-
-# 제목 포함영역 추출하기
-data2 = data1.findAll("a", {"class": "title"})
-# pprint(data2)
-
-# 텍스트만 추출 1
-# title_list = []
-# for t in data2:
-#     title_list.append(t.text)
-#
-# pprint(title_list)
-
-# 텍스트만 추출 2
-title_list = [t.text for t in data2]
-pprint(title_list)
+# 요일별 웹툰영역 추출하기
+data1_list = soup.findAll("div", {"class": "col_inner"})
+pprint(data1_list)
