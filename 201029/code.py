97095ec3a43adc353808ea0157a864334675c2e7
AnKiWoong <ankiwoong@gmail.com>
2020-10-29 08:51:51 +0900

    네이버 웹툰 제목 가져오기 - 제목 영역 코드 추출하기

--- 201029/code.py
+++ 201029/code.py
@@ -9,4 +9,8 @@ html.close()
 
 # 월요웹툰영역 추출하기
 data1 = soup.find("div", {"class": "col_inner"})
-pprint(data1)
+# pprint(data1)
+
+# 제목 포함영역 추출하기
+data2 = data1.findAll("a", {"class": "title"})
+pprint(data2)
