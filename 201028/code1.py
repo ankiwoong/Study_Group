7e88b26e25450c44df5936265d05b57768be9d6f
AnKiWoong <ankiwoong@gmail.com>
2020-10-28 20:56:52 +0900

    네이버 날씨 미세먼지 가져오기 - 내부 텍스트 추출

--- 201028/code1.py
+++ 201028/code1.py
@@ -12,4 +12,7 @@ data1 = soup.find("div", {"class": "detail_box"})
 # pprint(data1)
 
 data2 = data1.findAll("dd")
-pprint(data2)
+# pprint(data2)
+
+fine_dust = data2[0].find("span", {"class": "num"}).text
+print(fine_dust)
