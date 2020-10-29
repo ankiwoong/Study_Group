75c8a89063f2ad650158013fab037ab9aa26e91c
AnKiWoong <ankiwoong@gmail.com>
2020-10-30 07:47:11 +0900

    오늘의 영어회화 (해커스 영어)

--- 201026/21.project.py
+++ 201026/21.project.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 def create_soup(url):
@@ -95,7 +96,30 @@ def scrape_it_news():
     print()
 
 
+def scrape_english():
+    print("[오늘의 영어 회화]")
+    url = "https://www.hackers.co.kr/?c=s_eng/eng_contents/I_others_english&keywd=haceng_submain_lnb_eng_I_others_english&logger_kw=haceng_submain_lnb_eng_I_others_english"
+
+    soup = create_soup(url)
+
+    sentences = soup.find_all("div", attrs={"id": re.compile("^conv_kor_t")})
+
+    print("(영어 지문)")
+    for sentence in sentences[
+        len(sentences) // 2 :
+    ]:  # 8문장이 있다고 가정할 때, index  rlwns 4 ~ 7 까지 잘라서 가져옴
+        print(sentence.get_text().strip())
+    print()
+
+    print("(한글 지문)")
+    for sentence in sentences[
+        : len(sentences) // 2
+    ]:  # 8문장이 있다고 가정할 때, index  rlwns 0 ~ 3 까지 잘라서 가져옴
+        print(sentence.get_text().strip())
+
+
 if __name__ == "__main__":
     # scrape_weather()  # 오늘의 날씨 정보 가져오기
     # scrape_headlind_news()  # 헤드라인 뉴스 정보 가져오기
-    scrape_it_news()
+    # scrape_it_news()  # IT 뉴스 정보 가져오기
+    scrape_english()  # 오늘의 영어 회화 가져오기
